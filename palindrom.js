8bdf940ceec05bdc88cf4839f80e1f9302bd32c7
mikbolshakov <107908985+mikbolshakov@users.noreply.github.com>
2022-09-27 18:03:38 +0300

    dev

---
+++ palindrom.js
@@ -0,0 +1,14 @@
+function palindrom(word) {
+  word = word.toLowerCase();
+  for (let i = 0; i < word.length / 2; i++) {
+    if (word[i] !== word[word.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function bestPalindrom(word) {
+  word = word.toLowerCase();
+  return word === word.split("").reverse().join("")
+}
